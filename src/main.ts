28e8882c498c7cbb03d4719357da509846f1cfbf
BahhTee <85681712+Bakhtiyor7@users.noreply.github.com>
2025-07-31 22:37:41 +0900

    feat: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as process from 'node:process';
-import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as process from 'node:process'
+import { Logger } from '@nestjs/common'
 
 async function bootstrap() {
-  const port = process.env.PORT ?? 3001;
-  const app = await NestFactory.create(AppModule);
-  await app.listen(port);
-  Logger.log(`App listening on port ${port}`);
+    const port = process.env.PORT ?? 3001
+    const app = await NestFactory.create(AppModule)
+
+    app.enableCors()
+
+    await app.listen(port)
+    Logger.log(`App listening on port ${port}`)
 }
-bootstrap();
+bootstrap()
